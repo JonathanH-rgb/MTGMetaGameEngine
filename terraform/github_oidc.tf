b9eb1d55459be3c2871a958879b0c268a097a358
jonathan <joni7840850@gmail.com>
2026-07-17 15:15:50 -0600

    add oidc infra for github actions to be able to push images to ECR

---
+++ terraform/github_oidc.tf
@@ -0,0 +1,73 @@
+#In this file we want to provide the github actions pipeline the posibility to push the 
+#image to ECR (aws container repository) for that we are going to create a IAM role to be assumed by my github 
+#pipeline, the mechanism for aws to recognize that is my piepline is the oidc protocol
+
+variable "github_repository" {
+  description = "GitHub repo allowed to assume the CI role, as \"owner/name\"."
+  type        = string
+  default = "JonathanH-rgb/mtgMetaGameEngine"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"] }
+
+data "aws_iam_policy_document" "github_actions_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repository}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${var.project_name}-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
+}
+
+data "aws_iam_policy_document" "ecr_push" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+    resources = [aws_ecr_repository.app.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name   = "ecr-push"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.ecr_push.json
+}
+
+output "github_actions_role_arn" {
+  description = "IAM role ARN for GitHub Actions to assume via OIDC."
+  value       = aws_iam_role.github_actions.arn
+}
