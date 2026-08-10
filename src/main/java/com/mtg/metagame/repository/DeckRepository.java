693cb78481f8ad4be7e60921d57b7c47946f0163
jonathan <joni7840850@gmail.com>
2026-08-10 09:15:22 -0600

    Add find by name method

--- src/main/java/com/mtg/metagame/repository/DeckRepository.java
+++ src/main/java/com/mtg/metagame/repository/DeckRepository.java
@@ -4,4 +4,6 @@ import com.mtg.metagame.domain.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeckRepository extends JpaRepository<Deck, Long> {
+
+    boolean existsByName(String name);
 }
